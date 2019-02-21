58ba207d46b52d2b7f99706edffd2e29f5a191d3
Arne Meier <Arne@iMac27i5.local>
2019-02-21 23:32:55 +0200

    10a, algus

--- 10a.py
+++ 10a.py
@@ -1,10 +1,27 @@
-import time
+import ast
 
-print ("juh")
+data = {}
+data_text = []
+
+with open('10a_demo.txt',"rt") as input_file:
+    for line in input_file:
+        data_text.append(line.rstrip('\n'))
+
+for element in data_text:
+    s = element.replace("position=<", "")
+    t = s.replace("> velocity=<", ",")
+    u = t.replace(" ", "")
+    print u.split(",")
+    # print element
+
+# kekk = ast.literal_eval(element)
+
+# print (data_text)
 
 
-print ('''
-Tere
-Minu
-Uus
-Vihik''')
+#
+# print ('''
+# Tere
+# Minu
+# Uus
+# Vihik''')
