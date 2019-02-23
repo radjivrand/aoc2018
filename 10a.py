a9685464265f91bfa7d10d7c9d5d758b9d9ba72d
Arne Meier <Arne@iMac27i5.local>
2019-02-24 01:18:50 +0200

    10 a ja b valmis!

--- 10a.py
+++ 10a.py
@@ -5,8 +5,9 @@ import time
 
 data = []
 data_text = []
+# veloce = []
 
-with open('10a_demo.txt',"rt") as input_file:
+with open('10_real.txt',"rt") as input_file:
     for line in input_file:
         data_text.append(line.rstrip('\n'))
 
@@ -18,30 +19,35 @@ for element in data_text:
     x = v.split(",")
     data.append(x)
 
-stars = np.chararray((31,31))
+input_data = np.array(data)
+normal_data = input_data.astype(int)
+
+position = np.hsplit(normal_data, 2)[0]
+veloce = np.hsplit(normal_data, 2)[1]
+
+for value in position:
+    value[0] = int(value[0])
+    value[1] = int(value[1])
+
+# print (position + veloce)
+
+smallest = 2000
+smallest_i = 0
+
+for i in range(0,10418):
+    position += veloce
+    # std_asi = np.std(position,axis=0)[1]
+    # if  std_asi < smallest:
+    #     # print ("on")
+    #     smallest_i = i
+
+print (position)
+
+stars = np.chararray((600,600))
 stars[:] = "."
-clear = stars
 
-for value in data:
-    value[0] = int(value[0]) + 10
-    value[1] = int(value[1]) + 10
+for value in position:
+    stars[value[1],value[0]] = "#"
 
-# for value in data:
-#     print (value[1])
-
-for value in data:
-    stars[value[0],value[1]] = "#"
-
-    # for dot in stars:
-    #     print (''.join(map(str, dot)))
-
-for i in range(0,5):
-    for value in data:
-        value[0] = int(value[0]) + int(value[2])
-        value[1] = int(value[1]) + int(value[3])
-        stars = clear
-        stars[value[0],value[1]] = "#"
-    for dot in stars:
-        print (''.join(map(str, dot)))
-    time.sleep(0.2)
-print (data)
+for dot in stars:
+    print (''.join(map(str, dot)))
