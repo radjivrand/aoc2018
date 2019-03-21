3d890912bfee7737c2c2726f9c9fd9af9b9959f9
Arne Meier <Arne@iMac27i5.local>
2019-03-21 23:53:15 +0200

    awwyiss, 12b solved!!

--- 12b_v3.py
+++ 12b_v3.py
@@ -2,13 +2,13 @@ import numpy as np
 import re
 import time
 ms = time.time()*1000.0
-with open('12a_input_demo_start.txt',"rt") as input_file:
+with open('12a_input_start.txt',"rt") as input_file:
     for line in input_file:
         start_list = list(line.rstrip('\n'))
 regex = r"(.....) => (.)"
 subst = "\\1 \\2"
 notes_list = []
-with open('12a_input_demo_notes.txt',"rt") as input_file:
+with open('12a_input_notes.txt',"rt") as input_file:
     for line in input_file:
         s = re.sub(regex, subst, line, 0, re.MULTILINE)
         s = s.split()
@@ -16,13 +16,7 @@ with open('12a_input_demo_notes.txt',"rt") as input_file:
             notes_list.append(list(s[0]))
 new_ms = time.time()*1000.0 - ms
 
-start_dict = {}
-for d in range(0,len(start_list)):
-    start_dict[d]=start_list[d]
-
-print (start_dict)
-
-def list_dots(input_list):
+def list_dots(input_list, prev_dots):
     counter = 0
     output_list = []
     for i in range(0,4):
@@ -30,25 +24,19 @@ def list_dots(input_list):
             output_list = ["."] * (4-i)
             # print ("+",i)
             break
-    for j in range(1,5):
-        # print (input_list[-1])
-
-        if input_list[-j] == '#':
-            output_end_list = ["."] * (5-j)
-            # print("chech")
-            break
-    return output_list + input_list + output_end_list
-
-# start_list = list(start)
-
-add_dots = ["."] * 4
-
-start_list = add_dots + start_list + add_dots
-
-# print (''.join(start_list))
-#
-# start_list = list_dots(start_list)
-print (''.join(start_list))
+    while input_list[-1] != "#":
+        del input_list[-1]
+    # for j in range(1,5):
+    #     # print (input_list[-1])
+    #
+    #     if input_list[-j] == '#':
+    output_end_list = ["."] * (4)
+    #         break
+    cur_dots = len(output_list) + prev_dots
+    return_list = []
+    return_list.append(output_list + input_list + output_end_list)
+    return_list.append(cur_dots)
+    return return_list
 
 def get_next_row(input_start_list, input_notes_list):
 # tsykkel mis viskab viiekaupa edasi ja leiab mis numbri ajal matchib
@@ -57,24 +45,41 @@ def get_next_row(input_start_list, input_notes_list):
     for i in range(0,len(input_start_list)):
         for nl in input_notes_list:
             if nl == input_start_list[i:i+5]:
-                # newlist.append("#")
                 flag = True
-            # break
         if flag == True:
             newlist.append("#")
         else:
             newlist.append(".")
         flag = False
-    # print (''.join(newlist))
+    newlist = ["."]*2 + newlist + ["."]*5
     return newlist
+
+dot_result = list_dots(start_list, 0)
+added_dots = dot_result[1]
+start_list = dot_result[0]
+
+for i in range(1,201):
+    start_list = get_next_row(start_list, notes_list)
+    dot_result = list_dots(start_list, added_dots)
+    added_dots = dot_result[1]
+    start_list = dot_result[0]
+
+    resultado = 0
+    for z in range(0, len(start_list)):
+        # print (start_list[z])
+        if start_list[z] == "#":
+            resultado += (z-added_dots)
+    print (resultado)
+
+print (80*(50000000000-200) + 17480)
 
 # def get_next_list(input_start_list, input_notes_list):
 #     for isl in range(0, len(input_start_list)):
-        # print (isl)
-        # for nl in input_notes_list:
-        #     if isl == nl:
-        #         print (key)
-
+#         print (isl)
+#         for nl in input_notes_list:
+#             if isl == nl:
+#                 print (key)
+#
 # get_next_list(start_list,notes_list)
 
 
