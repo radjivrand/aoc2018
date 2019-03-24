25163e632a14e374bbc67bca3d8af82d3aad3058
Arne Meier <Arne@iMac27i5.local>
2019-03-24 23:33:42 +0200

    start

---
+++ 13a.py
@@ -0,0 +1,5 @@
+# 1. laadida sisse kaart
+# 2. kaardi pealt lugeda välja, kus tüüp asub ning milline on tema suund
+# 3. tekitada algoritm suvalise tüübi jaoks
+# 4. panna kell tiksuma: vaadata kuhu läheb
+# 5. lasta kellal käia, kuni mingite tüüpide koordinaadid on samad.
